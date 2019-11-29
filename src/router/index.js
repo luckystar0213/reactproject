e96a0ebf1a3d6facf70d021f449dcf2fa648541f
BK1918weiqianqian <2628556303@qq.com>
2019-11-29 19:59:05 +0800

    项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,58 @@
+import {
+    Home,
+    Find,
+    Cart,
+    Mine,
+    Login
+} from "pages"
+
+
+export const TabBarRoute = [
+    {
+        path:"/home",
+        component:Home,
+        meta:{
+            flag:true
+        },
+        icon:"\ue628",
+        text:"首页"
+    },
+    {
+        path:"/find",
+        component:Find,
+        meta:{
+            flag:true
+        },
+        icon:"\ue663",
+        text:"发现"
+    },
+    {
+        path:"/cart",
+        component:Cart,
+        meta:{
+            flag:true
+        },
+        icon:"\ue737",
+        text:"购物车"
+    },
+    {
+        path:"/mine",
+        component:Mine,
+        meta:{
+            flag:true,
+            requiredAuth:true
+        },
+        icon:"\ue617",
+        text:"我的"
+    }
+];
+
+export const NoTabBarRoute = [
+    {
+        path:"/login",
+        component:Login,
+        meta:{}
+    }
+];
+
+export const RouteConfig = TabBarRoute.concat(NoTabBarRoute)
---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const proxy = require("http-proxy-middleware");
+module.exports  = (app)=>{
+    app.use("/appapi",proxy({
+        target:"http://www.mei.com",
+        changeOrigin:true,
+        pathRewrite:{
+            "^/ajax":""
+        }
+    }))
+}
