752ea9f2ded6ed7932449b31433e03639e2c8dfa
BK1918yanhongxia <1442962728@qq.com>
2019-12-05 09:37:56 +0800

    3

--- src/components/swiper/styled.js
+++ src/components/swiper/styled.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-// import styled from "styled-components";
-
-// export const SwiperCon=styled.div`
-//     width:100%;
-//     height:4.5rem;
-//     position:relative;
-//     .swiper-content{
-//         height:100%;
-//         position:absolute;
-//         left:0;
-//         li{
-//             width:100vw;
-//             height:100%;
-//             float:left;
-//         }
-//         img{
-//             width:100%;
-//             height:100%;
-//         }
-//     }
-
-// `
-
-=======
->>>>>>> aloye
 
 import styled from "styled-components";
 
@@ -33,18 +7,10 @@ export const SwiperCon = styled.div`
     position:relative;
     .swiper_box{
     width:100%;
-<<<<<<< HEAD
-    height:100%;
-=======
     height:3.3rem;
->>>>>>> aloye
     }
     img{
         width:100%;
         height:100%;
     }
-<<<<<<< HEAD
 `
-=======
-`
->>>>>>> aloye
